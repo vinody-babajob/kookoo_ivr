bd640d11f36ee157c5e79d27f7c5b2d9c9a06d8a
vinody <vinody@vinodys-MacBook-Pro.local>
2015-09-25 13:26:39 +0530

    added recording audio and playing to preferred person

--- index.php
+++ index.php
@@ -1,26 +1,72 @@
 <?php
 
 require dirname(__FILE__).'/KooKoo-PHP/kookoophp/response.php';//include response.php into your code
+session_start();
 
 $r = new response();
 $r->setFiller(true);
 
 if (isset($_REQUEST['event']) && $_REQUEST['event'] == 'NewCall') {
     $cd = new CollectDtmf(); //initiate new collect dtmf object
-    $cd->addPlayText("Press 1, for sales");
-    $cd->addPlayText("Press 2, for to know about our company");
+    $cd->setMaxDigits(15);
+    $cd->setTermChar('#');
+    $cd->addPlayText("Please enter number to send message end with hash!");
     $r->addCollectDtmf($cd);
-} elseif (isset($_REQUEST['event']) && $_REQUEST['event'] == 'GotDTMF') {
-    if (isset($_REQUEST['data']) && !empty($_REQUEST['data'])) {
-        $r->addPlayText("you have pressed D T M F" . $_REQUEST['data']);
+    $_SESSION['next_goto'] = 'phonemenu';
+} elseif ($_SESSION['next_goto'] == 'phonemenu' && isset($_REQUEST['event']) && $_REQUEST['event'] == 'GotDTMF') {
+    if (isset($_REQUEST['data']) && !empty($_REQUEST['data']) && strlne($_REQUEST['data']) == 10) {
+        
+
+        $_SESSION['pref_num']=$_REQUEST['data'];
+
+        $r->addPlayText('Please Record Your Message to send!');
+
+        //give unique file name for each recording
+		$r->addRecord('filename2','wav','120');
+
+        $_SESSION['next_goto'] == 'Record_Status';
+
     } else {
-        $r->addPlayText("you have not given any input please re enter");
+        $r->addPlayText("you have not given any input please re enter or wrong input");
+        
         $cd = new CollectDtmf(); //initiate new collect dtmf object
-        $cd->addPlayText("Press 1, for sales");
-        $cd->addPlayText("Press 2, for to know about our company");
-        $r->addCollectDtmf($cd);
+	    $cd->setMaxDigits(10);
+	    $cd->setTermChar('#');
+	    $cd->addPlayText("Please enter number to send message end with hash!");
+	    $r->addCollectDtmf($cd);
     }
-}else{
+} else if($_REQUEST['event'] == 'Record' && $_SESSION['next_goto'] == 'Record_Status' ) {
+//recorded file will be come as  url in data param
+//print parameter data value
+	 $r->addPlayText('your recorded audio is ');
+	 $_SESSION['record_url']=$_REQUEST['data'];
+	 $r->addPlayAudio($_SESSION['record_url']);
+	 $r->addPlayText('Thanks you for calling, we will deliver your message');
+
+	 $r->addDial($_SESSION['pref_num'],'true',1000,30,'ring');
+	 $_SESSION['next_goto'] = 'Dial1_Status';
+
+
+	 $r->addHangup();	
+} else if($_REQUEST['event'] == 'Dial' && $_SESSION['next_goto'] == 'Dial1_Status' ) {
+	//dial url will come data param  //if dial record false then data value will be -1 or null
+	//dial status will come in status (answered/not_answered) param
+	//print parameter data and status params value
+ 	 $_SESSION['dial_record_url']=$_REQUEST['data'];
+	 $_SESSION['dial_status']=$_REQUEST['status'];
+	 $_SESSION['dial_callduration']=$_REQUEST['callduration'];
+	 if($_REQUEST['status'] == 'not_answered'){
+		//if you would like dial another number, if first call not answered,
+		//	
+	 	$r->addHangup();
+	 }else{
+	 	$r->addPlayAudio($_SESSION['record_url']);
+	 	 $r->addPlayText('Thanks you for calling, ');
+	 	 $r->addHangup();	// do something more or send hang up to kookoo
+	// call is answered
+	 }
+	 
+} else{
 	$r->addHangup();
 }
 
