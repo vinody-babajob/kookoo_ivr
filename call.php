e14e3c5861df72f474bd61149d6af49ff0093f30
vinody <vinody@vinodys-MacBook-Pro.local>
2015-09-29 12:49:19 +0530

    added new endpoint to store content

--- call.php
+++ call.php
@@ -1,7 +1,70 @@
 <?php
+	/** Include PHPExcel */
+require dirname(__FILE__).'/PHPExcel/Classes/PHPExcel.php';
+require_once dirname(__FILE__).'/PHPExcel/Classes/PHPExcel/IOFactory.php';
 
-$r = $_SERVER['QUERY_STRING'];
+function getExcelObject($filename) {
+	$objPHPExcel = PHPExcel_IOFactory::load($filename);
+	$objPHPExcel->setActiveSheetIndex(0);
 
-error_log($r);
+	return $objPHPExcel;
+}
+
+function writeToExcel($filename, $objPHPExcel) {
+	$objWriter = new PHPExcel_Writer_Excel2007($objPHPExcel);
+	$objWriter->save($filename);
+}
+
+function getParseQueryArray($parseStr) {
+	$get_array = [];
+
+	if($parseStr !== '')
+		parse_str($get_string, $get_array);
+
+	return $get_array;
+}
+
+function storeDataToExcel($filename, $data) {
+	$objPHPExcel = getExcelObject($filename);
+
+	$row = $objPHPExcel->getActiveSheet()->getHighestRow()+1;
+
+	$colK = 'A';
+	// $objPHPExcel->getActiveSheet()->SetCellValue('F'.$row, $queryArray['CallSid']);
+
+	foreach ($data as $col => $value) {
+	    $objPHPExcel->getActiveSheet()->SetCellValue($colK.$row, $value);
+	    $colK++;
+	}
+
+	writeToExcel($filename, $objPHPExcel);
+}
+
+function storeVoiceData($queryArray) {
+	$filename = "voicemail.xlsx";
+	storeDataToExcel($filename, $queryArray);
+}
+
+function storeCallAttemptData($queryArray) {
+	$filename = "callattempt.xlsx";
+	storeDataToExcel($filename, $queryArray);
+}
+
+
+$queryStr = trim($_SERVER['QUERY_STRING']);
+error_log($queryStr);
+$queryArray = getParseQueryArray($queryStr);
+
+if ($queryArray) {
+
+	if ($queryArray['CallType'] === 'voicemail') {
+		storeVoiceData($queryArray);
+	} else if ($queryArray['CallType'] === 'call-attempt') {
+		storeCallAttemptData($queryArray);
+	}
+
+} else {
+	error_log("No Parameters Provided");
+}
 
 ?>
